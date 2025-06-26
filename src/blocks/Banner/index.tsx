a5ae3abcc2f10affe484178f8f5fe8ce5cdf365e
josh <urgosxd@gmail.com>
2025-06-26 12:27:29 -0500

    aoe

--- src/blocks/Banner/index.tsx
+++ src/blocks/Banner/index.tsx
@@ -6,5 +6,5 @@ interface Props extends BannerBlockType{
 
 export function BannerBlock(props: Props) {
   const { title, image } = props;
-  return <Banner title={title!} backgroundUrl={(image as Media).url!} />;
+  return <Banner title={title || " "} backgroundUrl={(image as Media).url || '/placeholder.svg'} />;
 } 
