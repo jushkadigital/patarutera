6e75896d7fea49e1fe10e3e6b4e77259c0a8d69b
josh <urgosxd@gmail.com>
2025-07-05 11:19:58 -0500

    aoe

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ import { getCachedGlobal } from "@/utilities/getGlobals"
 import Image from "next/image"
 import Link from "next/link"
 import { FooterColumns } from "./Footer2"
+import { SvgFacebook, SvgInstagram, SvgTiktok } from "./IconsSvg"
 
 interface Props  {
   destination: Destination[]
@@ -29,7 +30,7 @@ export default async function FooterBlock({destination}:Props) {
       </div>
 
       {/* Content */}
-      <div className="relative z-10 container mx-auto px-4 py-16 text-white">
+      <div className="relative z-10 container mx-auto px-10 lg:px-4 pt-16 text-white">
         
         <div className="flex flex-col lg:flex-row space-y-10 lg:space-x-10">
         <div>
@@ -43,10 +44,24 @@ export default async function FooterBlock({destination}:Props) {
             <FooterColumns columns={[navItems.find(ele=>ele.nameColumn == "Destinos")]}/>
           </div>
         </div>
+        
       </div>
 
+      <div className="relative h-[100px] w-full bg-[#000000]/50  flex flex-col justify-center items gap-y-2">
+      <div className=" flex flex-row justify-center items-center gap-x-10">
+      <SvgFacebook height={35} width={35}/>
+      <SvgInstagram height={35} width={35}/>
+      <SvgTiktok height={35} width={35}/>
+      </div>
+      <div className="text-white text-lg text-center">
+        Desarrollado por Jushka Digital
+      </div>
+      </div>
+
+      
+
       {/* Colorful Bottom Border */}
-      <div className="h-2 w-full grid grid-cols-4">
+      <div className="h-2 w-full grid grid-cols-4 relative">
         <div className="bg-blue-600"></div>
         <div className="bg-[#3eae64]"></div>
         <div className="bg-yellow-500"></div>
