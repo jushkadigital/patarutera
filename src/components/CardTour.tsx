b3cd8d5a6b7edb559012540e2a7e52aebd37f7ae
josh <urgosxd@gmail.com>
2025-07-29 17:33:51 -0500

    aoe

--- src/components/CardTour.tsx
+++ src/components/CardTour.tsx
@@ -27,7 +27,7 @@ export default function CardTour({ unitData, mode = "list" }: CardTourProps) {
 
   if (mode === "grid") {
     return (
-      <Card className="w-full max-w-[435px] mx-auto overflow-hidden rounded-3xl shadow-lg py-0 gap-3 h-[720px] lg:h-[clamp(190.44px,46vw,883.2px)] group hover:shadow-2xl transition-all duration-500 hover:shadow-blue-500/25">
+      <Card className="w-full max-w-[435px] mx-auto overflow-hidden rounded-3xl shadow-lg py-0 gap-3 h-[clamp(0px,166vw,1341.2px)] sm:h-[clamp(190.44px,74vw,883.2px)] lg:h-[clamp(190.44px,46vw,883.2px)] group hover:shadow-2xl transition-all duration-500 hover:shadow-blue-500/25">
           <Link href={`/tours/${unitData.slug}`} className="h-[63%]">
         <div className="relative h-full  w-full overflow-hidden">
           <div className="absolute top-2 left-2 sm:top-4 sm:left-4 z-10 w-fit">
@@ -54,31 +54,30 @@ export default function CardTour({ unitData, mode = "list" }: CardTourProps) {
         <CardContent className="px-4 sm:px-6 h-[37%]">
           {/* title */}
         <Link href={`/tours/${unitData.slug}`}>
-        <div className="flex justify-center items-center h-[clamp(12px,3vw,57.6px)]">
-            <h2 className="text-[#2970b7] lg:text-[clamp(5.52px,1.3vw,25.6px)] font-bold text-center leading-tight  multi-line-truncate multi-line-truncate-2">
+        <div className="flex justify-center items-center h-[clamp(0px,10.89vw,111.6px)] sm:h-[clamp(0px,3.19vw,111.6px)] lg:h-[clamp(12px,3vw,57.6px)]">
+          <h2 className="text-[#2970b7] text-[clamp(0px,4.83vw,49.46px)]  sm:text-[clamp(5.96px,1.9vw,24.96px)] lg:text-[clamp(5.52px,1.3vw,25.6px)] font-bold text-center leading-tight  multi-line-truncate multi-line-truncate-2">
             {unitData.title}
           </h2>
         </div>
-          
           </Link>
           {/* miniDescription */}
-          <div className="text-[#6a6a6a] text-xs mb-3">
-            <RichText data={unitData.miniDescription} enableGutter={false} className="min-h-[clamp(13.8px,3.3vw,64px)]! !my-1 prose-custom-lg  prose-pink"/>
+          <div className="text-[#6a6a6a]">
+            <RichText data={unitData.miniDescription} enableGutter={false} className="h-[clamp(0px,10.07vw,124px)] sm:h-[clamp(0px,6.07vw,124px)] lg:h-[clamp(13.8px,3.3vw,64px)]! !my-1 lg:!my-1 prose-custom-lg  prose-pink"/>
           </div>
 
-          <div className="flex flex-row flex-wrap justify-around lg:justify-between items-center sm:items-end mb-0 px-2 sm:px-10 gap-4 sm:gap-0">
+          <div className="flex flex-row flex-wrap justify-around lg:justify-around items-center sm:items-end mb-0 px-2  gap-0 sm:gap-0  mt-[clamp(0px,2.41vw,24.73px)] lg:mt-[clamp(2.7px,0.6vw,12.8px)]">
             <div className="text-center sm:text-left">
               {/* desde */}
-              <p className="text-[#6a6a6a] text-xs lg:text-[clamp(3.5px,0.86vw,16.64px)] mb-1">{unitData.Desde}</p>
+              <p className="text-[#6a6a6a]  text-[clamp(13.28px,3.2vw,33.84px)]  sm:text-[clamp(5.68px,1.5vw,33.84px)] lg:text-[clamp(3.5px,0.86vw,16.64px)] ">{unitData.Desde}</p>
               <div className="flex items-baseline justify-center sm:justify-start">
                 {/* price */}
-                <span className="text-[#2970b7] text-2xl lg:text-[clamp(7.78px,1.86vw,35.84px)] font-bold">S/. {unitData.price}</span>
+                <span className="text-[#2970b7] text-[clamp(0px,6.76vw,69.25px)] sm:text-[clamp(0px,2.76vw,69.25px)]  lg:text-[clamp(7.78px,1.86vw,35.84px)] font-bold">S/. {unitData.price}</span>
               </div>
               {/* Person desc */}
-              <p className="text-[#6a6a6a] text-xs lg:text-[clamp(3.03px,0.7vw,14.08px)]">{unitData["Person desc"]}</p>
+              <p className="text-[#6a6a6a] text-[clamp(0px,2.65vw,27.2px)] sm:text-[clamp(0px,1.15vw,27.2px)] lg:text-[clamp(3.03px,0.7vw,14.08px)]">{unitData["Person desc"]}</p>
             </div>
 
-            <div className="flex flex-col items-center justify-center gap-4 sm:gap-2 h-full">
+            <div className="flex flex-col items-center justify-center lg:gap-[clamp(0.2px,0.1vw,3px)] h-full">
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger asChild>
@@ -88,9 +87,9 @@ export default function CardTour({ unitData, mode = "list" }: CardTourProps) {
                         alt="Max Passengers"
                         width={0}
                         height={0}
-                      className="w-[clamp(4.9px,1.2vw,23px)] h-auto"
+                      className="w-[clamp(0px,4.33vw,44.2px)] sm:w-[clamp(0px,2.33vw,44.2px)] lg:w-[clamp(4.9px,1.2vw,23px)] h-auto"
                       />
-                      <span className="text-[#6a6a6a] text-xs lg:text-[clamp(3.03px,0.7vw,14.08px)] mt-1">Hasta {unitData.maxPassengers}</span>
+                      <span className="text-[#6a6a6a] text-[clamp(0px,2.65vw,27.20px)] sm:text-[clamp(0px,1.15vw,27.20px)] lg:text-[clamp(3.03px,0.7vw,14.08px)] mt-1">Hasta {unitData.maxPassengers}</span>
                     </div>
                   </TooltipTrigger>
                   <TooltipContent>
@@ -108,10 +107,10 @@ export default function CardTour({ unitData, mode = "list" }: CardTourProps) {
                         alt="Difficulty"
                         width={0}
                         height={0}
-                      className="w-[clamp(4.9px,1.2vw,23px)] h-auto"
+                      className="w-[clamp(0px,4.33vw,44.2px)] sm:w-[clamp(0px,2.33vw,44.2px)] lg:w-[clamp(4.9px,1.2vw,23px)] h-auto"
                         
                       />
-                      <span className="text-[#6a6a6a] text-xs lg:text-[clamp(3.03px,0.7vw,14.08px)] mt-1">{trad[unitData.difficulty]}</span>
+                      <span className="text-[#6a6a6a] text-[clamp(0px,2.65vw,27.20px)] sm:text-[clamp(0px,1.15vw,27.20px)] lg:text-[clamp(3.03px,0.7vw,14.08px)] mt-1">{trad[unitData.difficulty]}</span>
                     </div>
                   </TooltipTrigger>
                   <TooltipContent>
@@ -121,11 +120,16 @@ export default function CardTour({ unitData, mode = "list" }: CardTourProps) {
               </TooltipProvider>
             </div>
           </div>
-          <Link href={`/tours/${unitData.slug}`} className="w-full flex justify-center cursor-pointer">
-            <Button className="w-fit bg-[#3eae64] hover:bg-[#35a058] text-white font-semibold px-6 py-3 sm:px-3 sm:py-3 text-sm sm:text-[16px] rounded-full cursor-pointer">
+          <div className="w-full flex flex-row mt-[clamp(0px,4.1vw,42.04px)] sm:mt-[clamp(0px,1.5vw,42.04px)] lg:mt-[clamp(4.6px,1.13vw,21.76px)]">
+            <div className="w-1/2"></div>
+            <Link href={`/tours/${unitData.slug}`} className="w-full flex justify-center cursor-pointer w-1/2">
+           <Button className="w-fit h-fit bg-[#3eae64] hover:bg-[#35a058] text-white font-semibold  py-[clamp(0px,1.20vw,12.36px)] sm:py-[clamp(0px,0.70vw,12.36px)] lg:py-[clamp(1.38px,0.3vw,6.4px)] px-[clamp(0px,2.89vw,29.68px)]  sm:px-[clamp(0px,1.49vw,29.68px)] lg:px-[clamp(3.31px,0.8vw,15.36px)]  text-[clamp(0px,3.86vw,39.57px)] sm:text-[clamp(0px,1.86vw,39.57px)] lg:text-[clamp(4.4px,1vw,20.48px)] rounded-full cursor-pointer ">
               Ver Detalles
             </Button>
           </Link>
+
+          </div>
+          
         </CardContent>
 
       </Card>
