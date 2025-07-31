d5f99470f9723f412aa485e588c9d7ff32da5fb1
josh <urgosxd@gmail.com>
2025-07-31 10:49:35 -0500

    aoe

--- src/components/OfertaCard.tsx
+++ src/components/OfertaCard.tsx
@@ -32,7 +32,7 @@ export  function OfertaCardComponent({ color = "#79368C",price , perPerson, type
         <div className="z-30 w-3/4">
 
           <div className={`uppercase tracking-wider opacity-90 ${isBigSize ? 'text-[clamp(8.83px,2.1vw,40.96px)]':'text-[clamp(8.83px,3.1vw,50.96px)] lg:text-[clamp(8.83px,2.1vw,40.96px)]'} font-normal`}>{type}</div>
-          <div className={`font-extrabold ${isBigSize ? 'text-[clamp(13.24px,6.2vw,101.44px)] lg:text-[clamp(26.44px,6.4vw,122.88px)]':'text-[clamp(13.24px,5.2vw,91.44px)] lg:text-[clamp(13.24px,3.2vw,61.44px)]' } uppercase mb-3 multi-line-truncate multi-line-truncate-3 text-left ${isBigSize ? 'lg:leading-[clamp(10px,5vw,150px)]' : 'lg:leading-[clamp(5.4px,2.8vw,80.24px)]'}  max-w-full`}>{title}</div>
+          <div className={`font-extrabold ${isBigSize ? 'text-[clamp(13.24px,6.2vw,101.44px)] lg:text-[clamp(26.44px,6.4vw,122.88px)]':'text-[clamp(13.24px,5.2vw,91.44px)] lg:text-[clamp(13.24px,3.2vw,61.44px)]' } uppercase mb-3 multi-line-truncate multi-line-truncate-3 text-left ${isBigSize ? 'leading-[clamp(5.4px,4vw,100px)] sm:leading-[clamp(5.4px,6vw,100px)] lg:leading-[clamp(10px,5vw,150px)]' : 'leading-[clamp(5.4px,5.5vw,85px)] sm:leading-[clamp(5.4px,5.5vw,85px)] lg:leading-[clamp(5.4px,2.8vw,80.24px)]'}  max-w-full`}>{title}</div>
 
           {/* Barra de colores */}
           <div className="flex h-1 w-50 rounded-full overflow-hidden">
@@ -61,7 +61,7 @@ export  function OfertaCardComponent({ color = "#79368C",price , perPerson, type
             viewBox="0 0 351 201"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className={`${ isBigSize?'w-[300px]':'w-[230px] sm:w-[270px] md:w-[350px] lg:w-[190px] xl:w-[250px] 2xl:w-[280px]'} h-auto`}
+            className={`${ isBigSize?'w-[180px] sm:w-[230px] md:w-[330px] lg:w-[290px] xl:w-[300px] 2xl:w-[280px]':'w-[180px] sm:w-[230px] md:w-[330px] lg:w-[190px] xl:w-[250px] 2xl:w-[280px]'} h-auto`}
           >
             <path
               d="M0.00491333 97.3629C26.4851 119.367 55.519 138.386 86.6057 153.899C146.977 184.046 215.063 201 287.103 201C308.79 201 330.123 199.461 351 196.491V0C273.357 63.8383 174.012 102.142 65.7045 102.142C43.3839 102.142 21.4464 100.51 0 97.3629H0.00491333Z"
