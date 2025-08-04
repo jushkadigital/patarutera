52fe043620074d61bfcbddd6a940d29239d01d50
josh <urgosxd@gmail.com>
2025-08-04 09:44:24 -0500

    aoe

--- src/blocks/CarouselDestinos/index.tsx
+++ src/blocks/CarouselDestinos/index.tsx
@@ -12,12 +12,41 @@ export async function CarouselDestinos(props: Props) {
   const data = await response.json()
 
 
-  const dataMap = data.docs.map((doc: any) => ({
+  const dataMap = data.docs.map((doc: any) => {
+    if(!doc.imageDestination && !doc.backgroundDestination){
+return ({
+    title: doc.name,
+    button: "Ver Destino",
+    src: '/placeholder.svg',
+    bgImage:  '/placeholder.svg',
+  })
+
+    }
+    if(!doc.imageDestination){
+      return ({
+    title: doc.name,
+    button: "Ver Destino",
+    src: '/placeholder.svg',
+    bgImage:  doc.backgroundDestination.url,
+  })
+  
+    }
+    if(!doc.backgroundDestination){
+      return ({
+    title: doc.name,
+    button: "Ver Destino",
+    src:  doc.imageDestination.url,
+    bgImage:  '/placeholder.svg',
+  })
+  
+    }
+    return({
     title: doc.name,
     button: "Ver Destino",
     src:  doc.imageDestination.url,
     bgImage:  doc.backgroundDestination.url,
-  }))
+  })})
+  
   return <div className="w-full overflow-hidden ">
     <CarouselDestinosComponent slides={[...dataMap,...dataMap]} titleObj={titleObj}/>
   </div>
