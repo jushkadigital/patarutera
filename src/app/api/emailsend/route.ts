a919098af7ee96df2cd5fc5493b45b5ec7d5707f
josh <urgosxd@gmail.com>
2025-10-14 15:40:49 -0500

    aoe

---
+++ src/app/api/emailsend/route.ts
@@ -0,0 +1,29 @@
+
+import * as React from 'react';
+import { Email } from '../../../components/emails/email-template-send';
+import { Resend } from 'resend';
+
+
+export async function POST() {
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'ventas@patarutera.pe',
+      to: 'ventas@patarutera.pe',
+      subject: 'Hello world',
+      react: Email() as React.ReactNode,
+      //html: '<div> Hello Next</div>'
+    });
+
+    console.log(error)
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
--- src/app/api/ipn/route.ts
+++ src/app/api/ipn/route.ts
@@ -1,9 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import fetch from "node-fetch"
+import * as React from 'react';
+import { Email } from '../../../components/emails/email-template-send';
+import { Resend } from 'resend';
 
 
 export async function POST(request: NextRequest) {
   // const secret = request.headers.get("secret");
+
+  //const resend = new Resend(process.env.RESEND_API_KEY);
   const document = await request.text();
 
   // console.log(document)
@@ -15,8 +20,54 @@ export async function POST(request: NextRequest) {
 
   const finalDivide = divideResponse[1].split("-")[1]
 
+  const emailResponse = data[23].split("=")
 
-  console.log(finalDivide)
+  console.log(emailResponse)
+  /*
+   try {
+     const { data, error } = await resend.emails.send({
+       from: 'ventas@patarutera.pe',
+       to: 'ventas@patarutera.pe',
+       subject: 'Hello world',
+       react: Email() as React.ReactNode,
+       //html: '<div> Hello Next</div>'
+ 
+     });
+ 
+     console.log(error)
+ 
+     if (error) {
+       return Response.json({ error }, { status: 500 });
+     }
+ 
+     return Response.json({ data });
+   } catch (error) {
+ 
+   }
+ 
+ 
+ 
+   try {
+     const { data, error } = await resend.emails.send({
+       from: 'ventas@patarutera.pe',
+       to: 'urgosxd@gmail.com',
+       subject: 'Hello world',
+       react: Email() as React.ReactNode,
+       //html: '<div> Hello Next</div>'
+     });
+ 
+     console.log(error)
+ 
+     if (error) {
+       return Response.json({ error }, { status: 500 });
+     }
+ 
+     return Response.json({ data });
+   } catch (error) {
+ 
+   }
+ 
+ */
   return NextResponse.json({
     "message": "nice",
     "cache": "update"
