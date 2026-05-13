860f4d19f693ea82a7005ae7a6cd2be964ab74b6
josue <urgosxd@gmail.com>
2026-05-13 18:02:07 -0500

    ae

--- src/modules/account/templates/account-layout.tsx
+++ src/modules/account/templates/account-layout.tsx
@@ -17,7 +17,7 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
       <div className="flex-1 content-container h-full max-w-5xl mx-auto">
         <div className="rounded-2xl border border-gray-200 bg-white shadow-sm">
           <div className="grid grid-cols-1 small:grid-cols-[240px_1fr] gap-6 px-4 small:px-8 py-6 small:py-8">
-            <div className="rounded-xl border border-gray-200 bg-white p-4">
+            <div className="">
               {customer && <AccountNav customer={customer} />}
             </div>
             <div className="flex-1 rounded-xl border border-gray-200 bg-white p-4 small:p-6">
